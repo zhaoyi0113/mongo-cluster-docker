8152789fc3e209004b2c20cffe9ed0da8afee7a0
Yi Zhao <zhaoyi0113@gmail.com>
2017-07-09 11:23:31 +1000

    update comment

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,7 +6,7 @@ mongodb3=`ping -c 1 ${MONGO3} | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 
 port=${PORT:-27017}
 
-echo "Waiting for startup.."
+echo "Waiting for startup.. ${mongodb1}:${port}"
 until mongo --host ${mongodb1}:${port} --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)' &>/dev/null; do
   printf '.'
   sleep 1
